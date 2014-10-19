dc661d6fd9470795de5f4bf16d70bd8ba5b265f8
f4k <tusemociones@gmail.com>
2014-10-19 09:47:21 -0300

    Repo sync

--- carbon.mk
+++ carbon.mk
@@ -1,7 +1,7 @@
-$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
-
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gxx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
